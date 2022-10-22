76e9ce95b0cc9244cae8f5558f1b6f785d856b2a
lukrei <113136700+lukrei@users.noreply.github.com>
2022-10-22 14:13:35 +0200

    Add files via upload

---
+++ src/offsets.rs
@@ -0,0 +1,9 @@
+#![allow(non_upper_case_globals)]
+pub const dwTeam: u32 = 1052148;
+pub const dwGeld: u32 = 1201380;
+pub const dwLeben: u32 = 1185592;
+pub const dwAttack: u32 = 1250160;
+pub const dwCrossIdTeam: u32 = 1200916;
+pub const dwCrossId: u32 = 1200920;
+pub const timestamp: u64 = 1592498283;
+
